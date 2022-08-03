350fc77717572cfd360537dd94620a62f3c2c9a8
Luan Severo <luansevero@icloud.com>
2022-08-03 14:08:37 -0300

    [Refactor] Middlewares outside the object function

--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -1,58 +1,54 @@
 import connection from "../setup/database.js";
 import bycrypt from 'bcrypt';
 
+const isNewUser = async (req, res, next) => {
+    const { email } = req.body;
+    try {
+        const { rows: user } = await connectionquery(`SELECT * FROM users WHERE email=$1`, [email]);
+        if (user.length !== 0) { return res.sendStatus(409) };
+        next();
+    } catch (error) {
+        console.log("[Error] - haveCostumer Middleware");
+        return res.sendStatus(500);
+    };
+};
+const isTheSamePassword = async (req, res, next) => {
+    const { password, confirmPassword } = req.body;
+    try {
+        if (password !== confirmPassword) { return res.status(422).send('Senhas diferentes') };
+        next();
+    } catch (error) {
+        console.log("[Error] - samePassword Middleware")
+        return res.sendStatus(500);
+    };
+};
+const encryptingPassword = async (req, res, next) => {
+    const { password } = req.body;
+    try {
+        const passwordHash = bycrypt.hashSync(password, 10);
 
+        res.locals.passwordHash = passwordHash;
 
-const authMiddlewares = {
-    haveCustomer: async (req,res,next) => {
-        const { email } = req.body;
-        try{
-            const { rows:user } = await connection.query(`SELECT * FROM users WHERE email=$1`, [email]);
-            if(user.length !== 0){return res.sendStatus(409)};
-            next();
-        }catch(error){
-            console.log("[Error] - haveCostumer Middleware")
-            return res.sendStatus(500);
-        };
-    },
-    samePassword: async (req,res,next) => {
-        const {password, confirmPassword} = req.body;
-        try{
-            if(password !== confirmPassword){return res.status(422).send('Senhas diferentes')};
+        next();
+    } catch (error) {
+        console.log("[Error] - encryptingPassword Middleware")
+        return res.sendStatus(500);
+    };
+};
 
-            next();
-        }catch(error){
-            console.log("[Error] - samePassword Middleware")
-            return res.sendStatus(500);
-        };
-    },
-    encryptingPassword: async (req,res,next) => {
-        const { password } = req.body;
-        try{
-            const passwordHash = bycrypt.hashSync(password, 10);
-            
-            res.locals.passwordHash = passwordHash;
+const isTheUser = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const user = await connection.query(`SELECT id, password FROM users WHERE email=$1`, [email]);
+        const isPasswordRight = bycrypt.compareSync(password, user.password);
+        if (!user || !isPasswordRight) { return res.sendStatus(401) };
 
-            next();
-        }catch(error){
-            console.log("[Error] - encryptingPassword Middleware")
-            return res.sendStatus(500);
-        };
-    },
-    isTheUser: async (req,res,next) => {
-        const { email, password } = req.body;
-        try{
-            const user = await connection.query(`SELECT id, password FROM users WHERE email=$1`, [email]);
-            const isPasswordRight = bycrypt.compareSync(password, user.password);
-            if(!user || !isPasswordRight){return res.sendStatus(401)};
+        res.locals.userId = user.id
+        next();
+    } catch (error) {
+        console.log("[Error] - isPasswordRight Middleware")
+        return res.sendStatus(500);
+    };
+};
 
-            res.locals.userId = user.id
-            next();
-        }catch(error){
-            console.log("[Error] - isPasswordRight Middleware")
-            return res.sendStatus(500);
-        };
-    }
-}
-
-export default authMiddlewares;
+export { isNewUser, isTheSamePassword, encryptingPassword, isTheUser };
--- src/routes/authRouters.js
+++ src/routes/authRouters.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import validationMiddleware from "../middlewares/validations/schemaValidations.js";
-import authMiddlewares from "../middlewares/authMiddlewares.js"
+import { isNewUser, isTheSamePassword, encryptingPassword, isTheUser } from "../middlewares/authMiddlewares.js"
 import authController from "../controllers/authController.js";
 
 const authRouter = Router();
 
-authRouter.post("/signup", validationMiddleware('signup'));
-authRouter.post("/signin", authMiddlewares.isTheUser, authController.signIn);
+authRouter.post("/signup", validationMiddleware('signup'), isNewUser, isTheSamePassword, encryptingPassword, authController.signUp);
+authRouter.post("/signin", validationMiddleware('signin'), isTheUser, authController.signIn);
 
 export default authRouter;
