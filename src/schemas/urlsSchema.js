c4ca155f6dc98a05adddd00f34000fe13c6ab96e
Luan Severo <luansevero@icloud.com>
2022-08-04 23:27:35 -0300

    [Feat] JoiSchema for urls

---
+++ src/schemas/urlsSchema.js
@@ -0,0 +1,9 @@
+import joi from "joi";
+
+const urlExpression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+const urlSchema = joi.object({
+    url: joi.string().pattern(urlExpression).required()
+})
+
+export { urlSchema }
